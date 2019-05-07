d0bd63115ebe9be4ccf7c6a41cbd6a64b1589502
Suthatta Lindstrii <psuthatta@gmail.com>
2019-05-07 11:04:54 +0200

    adde week01

---
+++ week01/apps.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let myFavoriteBooks = ['its_about_a_boy',
+  "harry_potter_and_the_sorcerers_stone",
+  "docter_glas",
+  "game_of_throne_season1",
+  "the_lord_of_the_rings",
+  "่jane_eyre",
+  "little_women",
+  "the_hobbit",
+  "baby_massage",
+  "svenska_kok_book"];
+console.log(myFavoriteBooks);
+
