9896393062255af8dd521eeeff84e15dd92bd3dd
Suthatta Lindstrii <psuthatta@gmail.com>
2019-06-05 22:39:43 +0200

    add week03 homework

--- week03/step2-5.js
+++ week03/step2-5.js
@@ -1,14 +1,18 @@
 'use strict';
 
 function multiplyAll(arr) {
-  // eslint-disable-next-line
-  let product = 1;
-
-  // Replace this comment and the next line with your code
-  console.log(arr, product);
-
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+  // Only change code above this line
   return product;
 }
+
+// Modify values below to test your code
 
 const result = multiplyAll([[1, 2], [3, 4], [5, 6]]);
 console.log(result); // 720
--- week03/step2-6.js
+++ week03/step2-6.js
@@ -5,11 +5,13 @@ const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 function flattenArray2d(arr) {
   // Replace this comment and the next line with your code
+
   console.log(arr);
 }
 
 function flattenArray3d(arr) {
   // Replace this comment and the next line with your code
+
   console.log(arr);
 }
 
