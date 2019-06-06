ed7f9cb52e78a04c2cbdb4410cb862e03765039a
Suthatta Lindstrii <psuthatta@gmail.com>
2019-06-07 00:07:20 +0200

    add homework week03 n.3

--- week03/step3-bonus.js
+++ week03/step3-bonus.js
@@ -4,7 +4,14 @@ const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 function makeUnique(arr) {
   // Replace this comment and the next line with your code
+  let temp = {};
+  for (let i = 0; i < arr.length; i++)
+    temp[arr[i]] = true;
+  let newTemp = [];
+  for (let copy in temp)
+    newTemp.push(copy);
   console.log(arr);
+  return newTemp;
 }
 
 const uniqueValues = makeUnique(values);
